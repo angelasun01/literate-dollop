8b3ca3efabc6dbbc7e2323a2e056002a95266844
angelasun01 <77741050+angelasun01@users.noreply.github.com>
2021-03-08 09:48:39 -0500

    Create script3.js

---
+++ EXERCISE07/script3.js
@@ -0,0 +1,11 @@
+var first = document.querySelector("first");
+console.log(first);
+// 2. Print just the text of `first`.
+console.log(first.innerText);
+// 3. Print the HTML of `first`.
+
+// 4. Get the section tag from the page and save it to a variable.
+
+// 5. Print the text content of the section tag
+
+// 6. Print the HTML content of the section tag
